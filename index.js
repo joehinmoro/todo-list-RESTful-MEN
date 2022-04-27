c428a771f8143b7bfa229e7c65bfb06922aa501b
joehinmoro <joehinmoro@gmail.com>
2022-04-27 10:21:58 +0100

    completed basic config and install

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const express = require("express");
 const { join } = require("path");
 const methodOverride = require("method-override");
-const mongoose = require("mongoose");
+
+// import todo model
+const Todo = require("./models/todos");
+// console.log(Todo);
+// server app port number
+const portNum = 3000;
 
 // *** APP SETTINGS ***
 // execute express app
@@ -11,3 +16,23 @@ const app = express();
 app.set("view engine", "ejs");
 // set views directory
 app.set("views", join(__dirname, "views"));
+// set static files directory
+app.set(express.static(join(__dirname, "public")));
+
+// *** MIDDLEWARE ***
+// parse url-encoded [form data]
+app.use(express.urlencoded({ extended: true }));
+// override post req to enable certain http verb req
+app.use(methodOverride("_method"));
+
+// test request
+app.get("/", (req, res) => {
+    res.send("Hello World! def");
+});
+
+// *** TODOS RESTFUL ROUTES ****
+
+// start server and listen on port
+app.listen(portNum, () => {
+    console.log(`listening on port: ${portNum}`);
+});
---
+++ models/todos.js
@@ -0,0 +1,35 @@
+// require mongoose
+const mongoose = require("mongoose");
+
+// connect to mongoDB
+mongoose
+    .connect("mongodb://localhost:27017/todosDB")
+    .then(() => {
+        console.log("DB connection successful");
+    })
+    .catch((err) => {
+        console.log("DB connection error:", err);
+    });
+
+// define todos schema
+const todoSchema = new mongoose.Schema({
+    text: String,
+    isDone: {
+        type: Boolean,
+        default: false,
+    },
+    timeCreated: {
+        type: Date,
+        default: new Date(),
+    },
+    timeUpdated: {
+        type: Date,
+        default: null,
+    },
+});
+
+// define todos model
+const Todo = mongoose.model("Todo", todoSchema);
+
+// export todos model
+module.exports = Todo;
